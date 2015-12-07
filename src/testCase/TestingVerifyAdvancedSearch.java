995532c566229fcce2068b02914600182fef4b34
jakther <jakther@01250LT2368.ccm.bethco.local>
2015-12-07 12:33:53 -0500

    Code update on 12/07/2015 12.33 pm

--- src/testCase/TestingVerifyAdvancedSearch.java
+++ src/testCase/TestingVerifyAdvancedSearch.java
@@ -2,15 +2,20 @@ package testCase;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import pageObject.CommonObjects;
+import pageObject.CreateNewCasePage;
 import pageObject.SignInPage;
 
 public class TestingVerifyAdvancedSearch {
 	WebDriver driver;
-	CommonObjects common=new 	CommonObjects(driver); 
-	SignInPage sign=new SignInPage(driver);
+	static String caseNumber="00001288";
+	
+	
+	
+	
 	
 	@Test(priority=0)
 	public void openDriver() {
@@ -22,16 +27,36 @@ public class TestingVerifyAdvancedSearch {
 	
 	@Test(priority=1)
 	public void logIN() throws InterruptedException{
+		SignInPage sign=new SignInPage(driver);
 		Thread.sleep(2000);
 		sign.validLogin();
+		Thread.sleep(2000);
 		
 	}
 	
 	
 	@Test(priority=2)
-	public void searchingCase(){
-		common.insertTxtOnSearchTxtBoxAndClickGoButton("00001288");
+	public void  searchingCase() throws InterruptedException{
+		CommonObjects common=new CommonObjects(driver);
 		
+		Thread.sleep(2000);
+		common.insertTxtOnSearchTxtBoxAndClickGoButton(caseNumber);
+		
+	}
+	
+	
+	@Test(priority=3)
+	public void verifyOpenCase() throws InterruptedException {
+		Thread.sleep(2000);
+		CreateNewCasePage caseLabel=new CreateNewCasePage(driver);
+		String acctuelCaseLabel=caseLabel.getNewAccountLabel();
+		
+		Assert.assertEquals(acctuelCaseLabel,caseNumber);
+		//if(acctuelCaseLabel==caseNumber){
+		//	System.out.println("Case Number Match");
+		////}else{
+		//	System.out.println("Case Number Didnt Match.Case number found :"+acctuelCaseLabel+" insted of :"+caseNumber);
+		//}
 	}
 
 }
