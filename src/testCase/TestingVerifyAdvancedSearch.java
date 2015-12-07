56f053c358d2678a5c694364a14ba3012263412b
jakther <jakther@01250LT2368.ccm.bethco.local>
2015-12-07 12:47:51 -0500

    Code update on 12/07/2015 12:47 pm

--- src/testCase/TestingVerifyAdvancedSearch.java
+++ src/testCase/TestingVerifyAdvancedSearch.java
@@ -1,62 +1,55 @@
 package testCase;
 
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import pageObject.CommonObjects;
 import pageObject.CreateNewCasePage;
 import pageObject.SignInPage;
 
 public class TestingVerifyAdvancedSearch {
-	WebDriver driver;
-	static String caseNumber="00001288";
-	
-	
-	
-	
-	
-	@Test(priority=0)
-	public void openDriver() {
-		driver=new FirefoxDriver();
+	WebDriver driver = null;
+	static String caseNumber = "00001288";
+
+	@BeforeTest
+	public void tearup() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test(priority = 0)
+	public void openApplication() {
 		driver.get("https://test.salesforce.com/");
-		
+
 	}
-	
-	
-	@Test(priority=1)
-	public void logIN() throws InterruptedException{
-		SignInPage sign=new SignInPage(driver);
-		Thread.sleep(2000);
+
+	@Test(priority = 1)
+	public void logIN() {
+		SignInPage sign = new SignInPage(driver);
 		sign.validLogin();
-		Thread.sleep(2000);
-		
 	}
-	
-	
-	@Test(priority=2)
-	public void  searchingCase() throws InterruptedException{
-		CommonObjects common=new CommonObjects(driver);
-		
-		Thread.sleep(2000);
+
+	@Test(priority = 2)
+	public void searchingCase() {
+		CommonObjects common = new CommonObjects(driver);
 		common.insertTxtOnSearchTxtBoxAndClickGoButton(caseNumber);
-		
 	}
-	
-	
-	@Test(priority=3)
-	public void verifyOpenCase() throws InterruptedException {
-		Thread.sleep(2000);
-		CreateNewCasePage caseLabel=new CreateNewCasePage(driver);
-		String acctuelCaseLabel=caseLabel.getNewAccountLabel();
-		
-		Assert.assertEquals(acctuelCaseLabel,caseNumber);
-		//if(acctuelCaseLabel==caseNumber){
-		//	System.out.println("Case Number Match");
-		////}else{
-		//	System.out.println("Case Number Didnt Match.Case number found :"+acctuelCaseLabel+" insted of :"+caseNumber);
-		//}
+
+	@Test(priority = 3)
+	public void verifyOpenCase() {
+		CreateNewCasePage caseLabel = new CreateNewCasePage(driver);
+		String acctuelCaseLabel = caseLabel.getNewAccountLabel();
+		Assert.assertEquals(acctuelCaseLabel, caseNumber);
+	}
+
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+
 	}
 
 }
