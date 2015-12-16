a229108b4e3acdf778ed156275b241023c381889
jakther <jakther@01250LT2368.ccm.bethco.local>
2015-12-16 10:34:53 -0500

    Code Update at 12/16/2015 10:34 am

--- src/testCase/Calender.java
+++ src/testCase/Calender.java
@@ -14,30 +14,37 @@ public class Calender {
 		
 		WebDriver driver = new FirefoxDriver();
 		SignInPage sign = new SignInPage(driver);
-
+        String monthLooking="June 2016";
+        String dayLooking="24";
+        String[] monthLeftside=monthLooking.split(" ");
+        String fSplit=monthLeftside[0];
+        String sSplit=monthLeftside[1];
+        String verifyTitle=fSplit+" "+dayLooking+","+sSplit;
 		driver.get("https://test.salesforce.com/");
 		Thread.sleep(2000);
 		sign.validLogin();
 		Thread.sleep(3000);
+		System.out.println("Date Looking For:"+verifyTitle);
 		
-		
+		try{
 		
 		for(int i=0;i<12;i++){
 			List<WebElement> dateList=driver.findElements(By.cssSelector(".header>td"));		  
 			  WebElement getMonth=dateList.get(1);
 			  WebElement nextMonth=driver.findElement(By.cssSelector(".nextCalArrow"));
 				String monthVerify=getMonth.getText();
-				System.out.println(monthVerify);
-		if(monthVerify.equals("April 2016")){
-			System.out.println("Valid Month Found");
-		
+				//System.out.println(monthVerify);
+		if(monthVerify.equals(monthLooking)){
+			//System.out.println("Valid Month Found");
+		Thread.sleep(2000);
 		List<WebElement> dateList1=driver.findElements(By.cssSelector(".calActive"));
 		for(int d=0;d<dateList1.size();d++){
 		WebElement days=dateList1.get(d);
 		String daysText=days.getText();
 		if(days.isDisplayed())
-			if(daysText.equals("29")){
+			if(daysText.equals(dayLooking)){
 				days.click();
+				break;
 			}
 		}
 		
@@ -46,7 +53,25 @@ public class Calender {
 			Thread.sleep(1000);
 		}
 		
+		}}catch(IndexOutOfBoundsException a){}
+		
+		String title=driver.findElement(By.cssSelector(".dateText")).getText();
+		String []getTitle=title.split(" ");
+		String fTitle=getTitle[1];
+		String sTitle=getTitle[2];
+		String tTitle=getTitle[3];
+		String finalTitle=fTitle+" "+sTitle+tTitle;
+		System.out.println("Succesfully Date Found "+finalTitle);
+		//Thread.sleep(2000);
+		if(finalTitle.equals(verifyTitle)){
+			System.out.println("Test Pass");
+		}else{
+			System.out.println("Test Fail");
 		}
+		
+		
+		
+		
 		/*
 			
 			for(int i=0;i<12;i++){
