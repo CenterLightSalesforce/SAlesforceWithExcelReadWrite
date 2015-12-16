023e34ac90de346d970b8aae08cf3798a8a75f13
jakther <jakther@01250LT2368.ccm.bethco.local>
2015-12-16 09:58:33 -0500

    Code Update on 12/16/2015 9:58 am

--- src/testCase/Calender.java
+++ src/testCase/Calender.java
@@ -21,6 +21,33 @@ public class Calender {
 		Thread.sleep(3000);
 		
 		
+		
+		for(int i=0;i<12;i++){
+			List<WebElement> dateList=driver.findElements(By.cssSelector(".header>td"));		  
+			  WebElement getMonth=dateList.get(1);
+			  WebElement nextMonth=driver.findElement(By.cssSelector(".nextCalArrow"));
+				String monthVerify=getMonth.getText();
+				System.out.println(monthVerify);
+		if(monthVerify.equals("April 2016")){
+			System.out.println("Valid Month Found");
+		
+		List<WebElement> dateList1=driver.findElements(By.cssSelector(".calActive"));
+		for(int d=0;d<dateList1.size();d++){
+		WebElement days=dateList1.get(d);
+		String daysText=days.getText();
+		if(days.isDisplayed())
+			if(daysText.equals("29")){
+				days.click();
+			}
+		}
+		
+		}else{
+			nextMonth.click();
+			Thread.sleep(1000);
+		}
+		
+		}
+		/*
 			
 			for(int i=0;i<12;i++){
 				List<WebElement> dateList=driver.findElements(By.xpath(".//*[@id='homeCalendarSection']/div/div[2]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr"));		  
@@ -28,6 +55,20 @@ public class Calender {
 					String monthVerify=getMonth.getText();
 			if(monthVerify.equals("April 2016")){
 				System.out.println("Valid Month Found");
+				     
+				for(int y=0;y<5;y++){
+					
+					for(int z=0;z<7;z++){
+					WebElement days=driver.findElement(By.xpath(".//*[@id='homeCalendarSection']/div/div[2]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr["+(y+3)+"]/td["+(z+1)+"]/a"));
+					String dayText=days.getText();
+					if(days.isEnabled()){
+						if(dayText.equals("27")){
+						days.click();
+						break;
+						}
+					}
+					
+				}}
 				
 				
 				
@@ -37,7 +78,7 @@ public class Calender {
 				driver.findElement(By.xpath(".//*[@id='homeCalendarSection']/div/div[2]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr[1]/td[3]/a/img")).click();
 				Thread.sleep(1000);
 				}
-			}
+			}*/
 					}
 
 }
