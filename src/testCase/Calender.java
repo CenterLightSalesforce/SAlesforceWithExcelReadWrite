97608d6d1151ed9c49e8a21226afd24771c868fc
jakther <jakther@01250LT2368.ccm.bethco.local>
2015-12-22 11:41:33 -0500

    Code Update on 12/22/15 11:41 am

--- src/testCase/Calender.java
+++ src/testCase/Calender.java
@@ -15,84 +15,96 @@ import pageObject.SignInPage;
 import utilities.ExcelUtilis;
 
 public class Calender {
-	static int countMatchLanguage = 0;
-	static int countUnMatchLanguage = 0;
+	static int countMatchLanguage;
+	static int countUnMatchLanguage;
 	static String excelPath = "C:\\Users\\jakther\\Desktop\\jahed\\workspace\\SalesforceDemo\\src\\testData\\TestData.xlsx";
-	
-	
+
 	public static void main(String[] args) throws Exception {
 
 		WebDriver driver = null;
-	//	System.setProperty("webdriver.chrome.driver", "C:\\Users\\jakther\\Desktop\\jahed\\java\\chromedriver\\chromedriver.exe");
-		//driver = new ChromeDriver();
-		
-		//System.setProperty("webdriver.ie.driver", "C:\\IEDriverServer.exe");
-       /// driver=new InternetExplorerDriver(); 
-		 // System.setProperty("webdriver.ie.driver", "C:\\IEDriverServer.exe");
-	    //    DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();  
-	     ///   ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-	     //   WebDriver driver = new InternetExplorerDriver(ieCapabilities);
 
 		
-		 driver=new FirefoxDriver();
-		
-		
-		
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		SignInPage sign = new SignInPage(driver);
-		ExcelUtilis excel = new ExcelUtilis(driver);
-		excel.setExcelFile(excelPath, "Sheet2");
 
-		// String monthLooking="July 2016";
-		// String dayLooking="19";
-		// String[] monthLeftside=monthLooking.split(" ");
-		// String fSplit=monthLeftside[0];
-		// String sSplit=monthLeftside[1];
-		// String verifyTitle=fSplit+" "+dayLooking+","+sSplit;
-		driver.get("https://test.salesforce.com/");
-		Thread.sleep(2000);
-		sign.validLogin();
-		driver.manage().window().maximize();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath(".//*[@id='Account_Tab']/a")).click();
-		driver.findElement(By.xpath(".//*[@id='hotlist']/table/tbody/tr/td[2]/input")).click();
-		driver.findElement(By.xpath(".//*[@id='bottomButtonRow']/input[1]")).click();
+		for (int i = 0; i < 3; i++)
 
-		List<WebElement> listData = driver.findElements(By.xpath(".//*[@id='00NF000000ColUB']/option"));
-		// List<WebElement>
-		// listData=driver.findElements(By.tagName("select")).get(1);
-		for (int h = 1; h < listData.size(); h++) {
-			WebElement listText = driver.findElements(By.xpath(".//*[@id='00NF000000ColUB']/option")).get(h);
-			String text = listText.getText();
-			String getDataFromExcel = excel.getCellData(h - 1, 0);
-			System.out.println("Data From Application Data From Excel File");
+		{
+			countMatchLanguage = 0;
+			countUnMatchLanguage = 0;
 
-			if (text.length() < 8) {
-
-				System.out.println(text + "\t\t\t" + getDataFromExcel);
+			if (i == 0) {
+				driver = new FirefoxDriver();
+				System.out.println("Start Mozila Firefox");
+			} else if (i == 1) {
+				System.setProperty("webdriver.chrome.driver",
+						"C:\\Users\\jakther\\Desktop\\jahed\\java\\chromedriver\\chromedriver.exe");
+				driver = new ChromeDriver();
+				System.out.println("Start Google Chrome ");
+			} else if (i == 2) {
+				System.setProperty("webdriver.ie.driver", "C:\\IEDriverServer.exe");
+				DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
+				ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+						true);
+				driver = new InternetExplorerDriver(ieCapabilities);
+				System.out.println("Start Internet Explorar");
 			} else {
-				System.out.println(text + "\t\t" + getDataFromExcel);
+				break;
 			}
 
-			if (text.equals(getDataFromExcel)) {
-				countMatchLanguage = countMatchLanguage + 1;
-			} else {
-				countUnMatchLanguage = countUnMatchLanguage + 1;
-				System.out.println("Language Didnt Match On the Application. Language Found :" + text
-						+ " .But on The Excel Sheet Language are:" + getDataFromExcel);
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			SignInPage sign = new SignInPage(driver);
+			ExcelUtilis excel = new ExcelUtilis(driver);
+			excel.setExcelFile(excelPath, "Sheet2");
+
+			// String monthLooking="July 2016";
+			// String dayLooking="19";
+			// String[] monthLeftside=monthLooking.split(" ");
+			// String fSplit=monthLeftside[0];
+			// String sSplit=monthLeftside[1];
+			// String verifyTitle=fSplit+" "+dayLooking+","+sSplit;
+			driver.get("https://test.salesforce.com/");
+			Thread.sleep(2000);
+			sign.validLogin();
+			driver.manage().window().maximize();
+			Thread.sleep(3000);
+			driver.findElement(By.xpath(".//*[@id='Account_Tab']/a")).click();
+			driver.findElement(By.xpath(".//*[@id='hotlist']/table/tbody/tr/td[2]/input")).click();
+			driver.findElement(By.xpath(".//*[@id='bottomButtonRow']/input[1]")).click();
+
+			List<WebElement> listData = driver.findElements(By.xpath(".//*[@id='00NF000000ColUB']/option"));
+			// List<WebElement>
+			// listData=driver.findElements(By.tagName("select")).get(1);
+			for (int h = 1; h < listData.size(); h++) {
+				WebElement listText = driver.findElements(By.xpath(".//*[@id='00NF000000ColUB']/option")).get(h);
+				String text = listText.getText();
+				String getDataFromExcel = excel.getCellData(h - 1, 0);
+				System.out.println("Data From Application Data From Excel File");
+
+				if (text.length() < 8) {
+
+					System.out.println(text + "\t\t\t" + getDataFromExcel);
+				} else {
+					System.out.println(text + "\t\t" + getDataFromExcel);
+				}
+
+				if (text.equals(getDataFromExcel)) {
+					countMatchLanguage = countMatchLanguage + 1;
+				} else {
+					countUnMatchLanguage = countUnMatchLanguage + 1;
+					System.out.println("Language Didnt Match On the Application. Language Found :" + text
+							+ " .But on The Excel Sheet Language are:" + getDataFromExcel);
+				}
+
 			}
 
-		}
+			if (countMatchLanguage == listData.size()) {
+				System.out.println("All Language Match with Excel File");
+			} else {
+				System.out.println(countUnMatchLanguage + " Language Didnt Match with Excel File");
+			}
+			System.out.println(countMatchLanguage + " Language  Match with Excel File");
 
-		if (countMatchLanguage == listData.size()) {
-			System.out.println("All Language Match with Excel File");
-		} else {
-			System.out.println(countUnMatchLanguage + " Language Didnt Match with Excel File");
+			driver.close();
 		}
-		System.out.println(countMatchLanguage + " Language  Match with Excel File");
-		
-		
-		driver.close();
 		/*
 		 * System.out.println("Date Looking For:"+verifyTitle);
 		 * 
