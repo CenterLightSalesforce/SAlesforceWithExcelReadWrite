e9937955c55cd786fe37b7057dcea948f92cce4b
jakther <jakther@01250LT2368.ccm.bethco.local>
2015-12-22 13:13:05 -0500

    Code Update at 12/22/15 1:12 pm

--- src/testCase/Calender.java
+++ src/testCase/Calender.java
@@ -18,16 +18,15 @@ public class Calender {
 	static int countMatchLanguage;
 	static int countUnMatchLanguage;
 	static String excelPath = "C:\\Users\\jakther\\Desktop\\jahed\\workspace\\SalesforceDemo\\src\\testData\\TestData.xlsx";
-
+	static String verifyTitle=null;
 	public static void main(String[] args) throws Exception {
 
 		WebDriver driver = null;
 
 		
 
-		for (int i = 0; i < 3; i++)
-
-		{
+	for (int i = 0; i < 3; i++)
+	{
 			countMatchLanguage = 0;
 			countUnMatchLanguage = 0;
 
@@ -55,12 +54,7 @@ public class Calender {
 			ExcelUtilis excel = new ExcelUtilis(driver);
 			excel.setExcelFile(excelPath, "Sheet2");
 
-			// String monthLooking="July 2016";
-			// String dayLooking="19";
-			// String[] monthLeftside=monthLooking.split(" ");
-			// String fSplit=monthLeftside[0];
-			// String sSplit=monthLeftside[1];
-			// String verifyTitle=fSplit+" "+dayLooking+","+sSplit;
+			
 			driver.get("https://test.salesforce.com/");
 			Thread.sleep(2000);
 			sign.validLogin();
@@ -73,7 +67,8 @@ public class Calender {
 			List<WebElement> listData = driver.findElements(By.xpath(".//*[@id='00NF000000ColUB']/option"));
 			// List<WebElement>
 			// listData=driver.findElements(By.tagName("select")).get(1);
-			for (int h = 1; h < listData.size(); h++) {
+			for (int h = 1; h < listData.size(); h++)
+			{
 				WebElement listText = driver.findElements(By.xpath(".//*[@id='00NF000000ColUB']/option")).get(h);
 				String text = listText.getText();
 				String getDataFromExcel = excel.getCellData(h - 1, 0);
@@ -81,88 +76,133 @@ public class Calender {
 
 				if (text.length() < 8  ) {
 
-					System.out.println(text + "\t\t\t\t" +"||"+ getDataFromExcel);
+					System.out.println(text + "\t\t\t\t" +" ||"+ getDataFromExcel);
 				} else {
-					System.out.println(text + "\t\t\t" +"||"+ getDataFromExcel);
+					System.out.println(text + "\t\t\t" +" ||"+ getDataFromExcel);
 				}
 
-				if (text.equals(getDataFromExcel)) {
+				if (text.equals(getDataFromExcel))
+				{
 					countMatchLanguage = countMatchLanguage + 1;
 				} else {
 					countUnMatchLanguage = countUnMatchLanguage + 1;
 					System.err.println("Language Didnt Match On the Application. Language Found :" + text
 							+ " .But on The Excel Sheet Language are:" + getDataFromExcel);
-				}
+				      }
 
 			}
 
-			if (countMatchLanguage == listData.size()) {
+			if (countMatchLanguage == listData.size())
+		    {
 				System.out.println("All Language Match with Excel File");
 			} else {
 				System.out.println(countUnMatchLanguage + " Language Didnt Match with Excel File");
-			}
+			       }
 			System.out.println(countMatchLanguage + " Language  Match with Excel File");
-
-			driver.close();
-		}
-		/*
-		 * System.out.println("Date Looking For:"+verifyTitle);
-		 * 
-		 * try{
-		 * 
-		 * for(int i=0;i<12;i++){ List<WebElement>
-		 * dateList=driver.findElements(By.cssSelector(".header>td"));
-		 * WebElement getMonth=dateList.get(1); WebElement
-		 * nextMonth=driver.findElement(By.cssSelector(".nextCalArrow")); String
-		 * monthVerify=getMonth.getText(); //System.out.println(monthVerify);
-		 * if(monthVerify.equals(monthLooking)){ //System.out.println(
-		 * "Valid Month Found"); Thread.sleep(2000); List<WebElement>
-		 * dateList1=driver.findElements(By.cssSelector(".calActive")); for(int
-		 * d=0;d<dateList1.size();d++){ WebElement days=dateList1.get(d); String
-		 * daysText=days.getText(); if(days.isDisplayed())
-		 * if(daysText.equals(dayLooking)){ days.click(); break; } }
-		 * 
-		 * }else{ nextMonth.click(); Thread.sleep(1000); }
-		 * 
-		 * }}catch(IndexOutOfBoundsException a){}
-		 * 
-		 * String
-		 * title=driver.findElement(By.cssSelector(".dateText")).getText();
-		 * String []getTitle=title.split(" "); String fTitle=getTitle[1]; String
-		 * sTitle=getTitle[2]; String tTitle=getTitle[3]; String
-		 * finalTitle=fTitle+" "+sTitle+tTitle; System.out.println(
-		 * "Succesfully Date Found "+finalTitle); //Thread.sleep(2000);
-		 * if(finalTitle.equals(verifyTitle)){ System.out.println("Test Pass");
-		 * }else{ System.out.println("Test Fail"); }
-		 * 
-		 */
-
-		/*
-		 * 
-		 * for(int i=0;i<12;i++){ List<WebElement>
-		 * dateList=driver.findElements(By.xpath(
-		 * ".//*[@id='homeCalendarSection']/div/div[2]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr"
-		 * )); WebElement getMonth=dateList.get(0); String
-		 * monthVerify=getMonth.getText(); if(monthVerify.equals("April 2016")){
-		 * System.out.println("Valid Month Found");
-		 * 
-		 * for(int y=0;y<5;y++){
-		 * 
-		 * for(int z=0;z<7;z++){ WebElement days=driver.findElement(By.xpath(
-		 * ".//*[@id='homeCalendarSection']/div/div[2]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr["
-		 * +(y+3)+"]/td["+(z+1)+"]/a")); String dayText=days.getText();
-		 * if(days.isEnabled()){ if(dayText.equals("27")){ days.click(); break;
-		 * } }
-		 * 
-		 * }}
-		 * 
-		 * 
-		 * 
-		 * 
-		 * break; }else{ driver.findElement(By.xpath(
-		 * ".//*[@id='homeCalendarSection']/div/div[2]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr[1]/td[3]/a/img"
-		 * )).click(); Thread.sleep(1000); } }
-		 */
+		
+			
+			
+			System.out.println("End language verify");
+		driver.findElement(By.xpath(".//*[@id='home_Tab']/a")).click();
+		Thread.sleep(2000);
+		System.out.println("Click Home page");
+		 
+		  
+		  try{
+			     String monthLooking="July 2016";
+				 String dayLooking="19";
+				 String[] monthLeftside=monthLooking.split(" ");
+				 String fSplit=monthLeftside[0];
+				 String sSplit=monthLeftside[1];
+				  verifyTitle=fSplit+" "+dayLooking+","+sSplit;
+				
+				 System.out.println("Date Looking For:"+verifyTitle);
+				 System.out.println("Start loopoinf for month"); 
+		    for(int l=0;l<12;l++)
+		       { 
+		    	
+		        List<WebElement> dateList=driver.findElements(By.cssSelector(".header>td"));
+		        WebElement getMonth=dateList.get(1);
+		        WebElement  nextMonth=driver.findElement(By.cssSelector(".nextCalArrow"));
+		        String  monthVerify=getMonth.getText(); 
+		           if(monthVerify.equals(monthLooking))
+		            {
+		        	   System.out.println("Start Looking for Day");
+			          //System.out.println("Valid Month Found");
+			          Thread.sleep(2000);
+			          List<WebElement> dateList1=driver.findElements(By.cssSelector(".calActive"));
+			            for(int d=0;d<dateList1.size();d++)
+			                  {
+			    	             WebElement days=dateList1.get(d);
+			                     String daysText=days.getText();
+			                         if(days.isDisplayed() && daysText.equals(dayLooking))
+			                                {
+		                                      days.click();
+		            	                      System.out.println("day click");
+		            	                      break;
+		            	                    }
+			                  }
+		            }else
+		               {
+		       	         nextMonth.click(); Thread.sleep(1000);
+			           }
+		      } 
+		  		  
+		  }catch(IndexOutOfBoundsException a)
+		     {
+			  throw(a);
+		      }
+		  
+		  System.out.println("start Test case ");
+		  try{
+			  System.out.println("verify date and month");
+		  String  title=driver.findElement(By.cssSelector(".dateText")).getText();
+		  String []getTitle=title.split(" ");
+		  String fTitle=getTitle[1];
+		  String sTitle=getTitle[2];
+		  String tTitle=getTitle[3];
+		  String  finalTitle=fTitle+" "+sTitle+tTitle;
+		  System.out.println( "Succesfully Date Found "+finalTitle); 
+		  //Thread.sleep(2000);
+		  //System.out.println("");
+		            if(finalTitle.equals(verifyTitle))
+		              {
+			            System.out.println("Test Pass");
+		              }else
+		                   {
+		        	    System.out.println("Test Fail");
+		        	       }
+		            driver.close(); 
+		       
+		    } catch(IndexOutOfBoundsException a)
+		        {
+		    	   throw(a);
+		    	}
+		  		  
+		 /* for(int m=0;m<12;m++){ 
+			  List<WebElement>  dateList=driver.findElements(By.xpath(".//*[@id='homeCalendarSection']/div/div[2]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr")); 
+		  WebElement getMonth=dateList.get(0); String
+		  monthVerify=getMonth.getText();
+		  if(monthVerify.equals("April 2016")){
+		  System.out.println("Valid Month Found");
+		  		  for(int y=0;y<5;y++){
+		  		  for(int z=0;z<7;z++){
+			  WebElement days=driver.findElement(By.xpath(".//*[@id='homeCalendarSection']/div/div[2]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr["+(y+3)+"]/td["+(z+1)+"]/a")); String dayText=days.getText();
+		            if(days.isEnabled())
+		               { 
+		            	  if(dayText.equals("27"))
+		            	     {
+		            		  days.click();
+		            		  break;
+		                         }}}}
+		  	  		       break;
+		        }else
+		        {
+		      driver.findElement(By.xpath(".//*[@id='homeCalendarSection']/div/div[2]/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr[1]/td[3]/a/img")).click(); 
+		      Thread.sleep(1000);
+		      }
+		  }*/
+		      
 	}
-
+  }
 }
